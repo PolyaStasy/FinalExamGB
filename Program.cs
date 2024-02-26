434e4b755878b9dc39fd029b4ac7b8d771ace353
Polina <idaya7s@mail.ru>
2024-02-26 15:22:15 +0300

    create new C# file and code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿
+﻿string [] STR = new string[] {"Hello", "2", "world", ";-)"};
 
+int N = STR.Length;
+int count = 0;
+string [] NewArray = new string[STR.Length];
+for(int i = 0; i<N; i++)
+{
+    if(STR[i].Length <= 3)
+    {
+       NewArray[count] = STR[i];
+       count ++; 
+    }  
+}
--- readme.md
+++ readme.md
@@ -9,3 +9,4 @@
 >[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 >
 >[“Russia”, “Denmark”, “Kazan”] → []
+
